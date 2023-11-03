738f697831787fb73cd885ca4c748f56119a8896
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2023-11-03 19:00:49 +0000

    added missing ;

--- src/test/com/alex/Dec2HexTestRunner.java
+++ src/test/com/alex/Dec2HexTestRunner.java
@@ -9,16 +9,22 @@ public class Dec2HexTestRunner {
   }
 
   static void testDecimalToHex() {
-    System.out.println("Check 255 = FF")
-    assert "FF".equals(Dec2Hex.decimalToHex(255)) : "Expected FF, but got " + Dec2Hex.decimalToHex(255);
+    System.out.println("Check 255 = FF");
+    assert "FF".equals(Dec2Hex.decimalToHex(255)) : "Expected FF, but got " +
+    Dec2Hex.decimalToHex(255);
 
-    System.out.println("Check 0 = 0")
-    assert "0".equals(Dec2Hex.decimalToHex(0)) : "Expected 0, but got " + Dec2Hex.decimalToHex(0);
+    System.out.println("Check 0 = 0");
+    assert "0".equals(Dec2Hex.decimalToHex(0)) : "Expected 0, but got " +
+    Dec2Hex.decimalToHex(0);
 
-    System.out.println("Check max int value = 7FFFFFFF")
-    assert "7FFFFFFF".equals(Dec2Hex.decimalToHex(Integer.MAX_VALUE)) : "Expected 7FFFFFFF, but got " + Dec2Hex.decimalToHex(Integer.MAX_VALUE);
+    System.out.println("Check max int value = 7FFFFFFF");
+    assert "7FFFFFFF".equals(
+        Dec2Hex.decimalToHex(Integer.MAX_VALUE)
+      ) : "Expected 7FFFFFFF, but got " +
+    Dec2Hex.decimalToHex(Integer.MAX_VALUE);
 
-    System.out.println("Check 100 = 64")
-    assert "64".equals(Dec2Hex.decimalToHex(100)) : "Expected 64, but got " + Dec2Hex.decimalToHex(100);
+    System.out.println("Check 100 = 64");
+    assert "64".equals(Dec2Hex.decimalToHex(100)) : "Expected 64, but got " +
+    Dec2Hex.decimalToHex(100);
   }
 }
