36051e29c217096dd6e831b036d63125bf37c003
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2023-11-03 17:34:02 +0000

    changed tests to use no external library

---
+++ src/test/com/alex/Dec2HexTestRunner.java
@@ -0,0 +1,10 @@
+public class Dec2HexTestRunner {
+    public static void main(String[] args) {
+        testConvertToHex();
+    }
+
+    static void testConvertToHex() {
+        String result = Dec2Hex.convertToHex(255);
+        assert "FF".equals(result) : "Expected FF, but got " + result;
+    }
+}
