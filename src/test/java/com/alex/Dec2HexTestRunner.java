d254ab84af32b0dc8e9032f1b2e53bdbaf4b7d7d
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2023-11-03 19:58:15 +0000

    converted tests to junit

--- src/test/java/com/alex/Dec2HexTestRunner.java
+++ src/test/java/com/alex/Dec2HexTestRunner.java
@@ -1,65 +1,66 @@
 package com.alex;
 
-import com.alex.Dec2Hex;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
 
-public class Dec2HexTestRunner {
+public class Dec2HexTest {
 
-  public static void main(String[] args) {
-    System.out.println("\nRunning tests...");
-
-    System.out.println("Testing decimal to hexadecimal conversion...");
-    testConvertToHex();
-
-    System.out.println("Testing no arguments...");
-    testNoArguments();
-
-    System.out.println("All tests passed!\n");
-  }
-
-  static void testConvertToHex() {
+  @Test
+  public void testConvertToHex() {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     PrintStream originalOut = System.out;
     System.setOut(new PrintStream(outContent));
 
     Dec2Hex.main(new String[] { "255" });
-    assert "Converting the Decimal Value 255 to Hex...\nHexadecimal representation is: FF".equals(
-        outContent.toString().trim()
-      ) : "Expected FF, but got " + outContent.toString();
+    assertEquals(
+      "Converting the Decimal Value 255 to Hex...\nHexadecimal representation is: FF",
+      outContent.toString().trim()
+    );
 
     outContent.reset();
     Dec2Hex.main(new String[] { "0" });
-    assert "Converting the Decimal Value 0 to Hex...\nHexadecimal representation is: 0".equals(
-        outContent.toString().trim()
-      ) : "Expected 0, but got " + outContent.toString();
+    assertEquals(
+      "Converting the Decimal Value 0 to Hex...\nHexadecimal representation is: 0",
+      outContent.toString().trim()
+    );
 
     outContent.reset();
     Dec2Hex.main(new String[] { "2147483647" });
-    assert "Converting the Decimal Value 2147483647 to Hex...\nHexadecimal representation is: 7FFFFFFF".equals(
-        outContent.toString().trim()
-      ) : "Expected 7FFFFFFF, but got " + outContent.toString();
+    assertEquals(
+      "Converting the Decimal Value 2147483647 to Hex...\nHexadecimal representation is: 7FFFFFFF",
+      outContent.toString().trim()
+    );
 
     outContent.reset();
     Dec2Hex.main(new String[] { "100" });
-    assert "Converting the Decimal Value 100 to Hex...\nHexadecimal representation is: 64".equals(
-        outContent.toString().trim()
-      ) : "Expected 64, but got " + outContent.toString();
+    assertEquals(
+      "Converting the Decimal Value 100 to Hex...\nHexadecimal representation is: 64",
+      outContent.toString().trim()
+    );
 
     System.setOut(originalOut);
   }
 
-  static void testNoArguments() {
+  @Test
+  public void testNoArguments() {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     PrintStream originalOut = System.out;
     System.setOut(new PrintStream(outContent));
 
-    try {
+    Exception exception = assertThrows(
+      IllegalArgumentException.class,
+      () -> {
         Dec2Hex.main(new String[] {});
-        assert false : "Expected an exception to be thrown";
-    } catch (IllegalArgumentException e) {
-        assert "Error: Please enter one decimal number to be processed.".equals(outContent.toString().trim()) : "Expected error message, but got " + outContent.toString();
-    }
+      }
+    );
+
+    assertEquals(
+      "Error: Please enter one decimal number to be processed.",
+      exception.getMessage()
+    );
 
     System.setOut(originalOut);
   }
