ac1dd394b2e15108fe2bd43e47351277fb8c815e
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2023-11-03 17:59:13 +0000

    added package to runner

--- src/test/com/alex/Dec2HexTestRunner.java
+++ src/test/com/alex/Dec2HexTestRunner.java
@@ -1,12 +1,15 @@
+package com.alex;
+
 import com.alex.Dec2Hex;
 
 public class Dec2HexTestRunner {
-    public static void main(String[] args) {
-        testConvertToHex();
-    }
 
-    static void testConvertToHex() {
-        String result = Dec2Hex.decimalToHex(255);
-        assert "FF".equals(result) : "Expected FF, but got " + result;
-    }
+  public static void main(String[] args) {
+    testConvertToHex();
+  }
+
+  static void testConvertToHex() {
+    String result = Dec2Hex.decimalToHex(255);
+    assert "FF".equals(result) : "Expected FF, but got " + result;
+  }
 }
